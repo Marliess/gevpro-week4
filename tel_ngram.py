d0428700910429c4116e74b35551328034701794
Marliess <liezq@live.nl>
2015-03-10 12:27:12 +0100

    Counting ngrams

--- tel_ngram.py
+++ tel_ngram.py
@@ -3,14 +3,20 @@
 import sys
 from collections import Counter
 from PyQt4 import QtGui, QtCore
+import time
 #thanks to: http://pyqt.sourceforge.net/Docs/PyQt4/qlayout.html
 
 class wordFreqUI(QtGui.QWidget):
+    """Constructor"""
     def __init__(self):
         super(wordFreqUI,self).__init__()
         self.initUI()
 
     def initUI(self):
+        """Create a screen with a combobox where the user can
+        choose between Bigrams and Unigrams. Create a button in
+        the screen where a file can be selected by the user to find
+        the Bigrams and Unigrams from this file."""
         self.box = QtGui.QVBoxLayout()
         self.setLayout(self.box)
         self.combox = QtGui.QComboBox(self)
@@ -26,6 +32,8 @@ class wordFreqUI(QtGui.QWidget):
 
         
     def eventHandler(self):
+        """Returns a 1 if the user want a Unigrams, return a 2
+        if the user want Bigrams. """
         if self.combox.currentText() == "Unigrams":
             self.x = 1
         else:
@@ -36,14 +44,22 @@ class wordFreqUI(QtGui.QWidget):
     def updateUniBi(self):
         # open file: http://stackoverflow.com/questions/19996128/qtgui-qfiledialog-unicode-designations
         userFile = QtGui.QFileDialog.getOpenFileName(self)
+        """The user wants a Unigram(self.x == 1). File will be openend
+        and with Counter and most_common() we get the 20 most frequent
+        unigrams. These are put in a string and will be displayed on the
+        screen. Align right so the frequencies will be displayed among
+        each other"""
         if self.x == 1:
             uniCount = Counter()
             for line in open(userFile):
                 uniCount.update(line.split())
-            for item in uniCount.most_common(20):
-                lbl = QtGui.QLabel(str(item[0]+' '+str(item[1])),self)
-                lbl.setAlignment(QtCore.Qt.AlignRight)
-                self.box.addWidget(lbl)
+                count = 1
+            for (i,j) in uniCount.most_common(20):
+                lbl = QtGui.QLabel(i,self)
+                lvl = QtGui.QLabel(str(j),self)
+                self.box.addWidget(lbl), self.box.addWidget(lvl)
+                #lbl = QtGui.QLabel(str(item[0]+' '+str(item[1])),self)
+                #self.box.addWidget(lbl)
         else:
             biCount = Counter()
             biDictio = {}
@@ -51,15 +67,17 @@ class wordFreqUI(QtGui.QWidget):
                 bi = len(line.split())
                 words = line.split()
                 for i in range(bi-1):
-                    biOpmaak = ''.join(words[:2])
+                    """Select all bigrams in a sentence, except the last words"""
+                    biSelect = ''.join(words[:2])
                     biGram = []
-                    biGram.append(biOpmaak)
+                    """Add the bigrams to the list biGram"""
+                    biGram.append(biSelect)
+                    """Update the most frequent bigrams"""
                     biCount.update(biGram)
-                    biDictio[biOpmaak] = words[:2]
+                    biDictio[biSelect] = words[:2]
                     #words.pop(0) niet nodig om RT @... te laten zien?
             for item in biCount.most_common(20):
-                lbl = QtGui.QLabel(' '.join(biDict.get(str(item[0])))+' '+str(item[1]),self)
-                lbl.setAlignment(QtCore.Qt.AlignRight)
+                lbl = QtGui.QLabel(' '.join(biDictio.get(str(item[0])))+' '+str(item[1]),self)
                 self.box.addWidget(lbl)
 
         
