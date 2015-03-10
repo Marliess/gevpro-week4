a000d72d372bc6e73efab2113e5b068a43ae0037
Marliess <liezq@live.nl>
2015-03-10 10:58:02 +0100

    Counting unigrams

--- tel_unigram.py
+++ tel_unigram.py
@@ -9,6 +9,7 @@ from PyQt4.QtGui import QLabel
 
 
 class wordFreqUI(QtGui.QWidget):
+    """Constructor"""
     def __init__(self):
         super(wordFreqUI,self).__init__()
         self.initUI()
@@ -18,20 +19,21 @@ class wordFreqUI(QtGui.QWidget):
         self.setLayout(grid)
         words = main(sys.argv)
         count = 1
+        """Put the words and their frequencies at the right places in the screen"""
         for (i,j) in words:
             self.lbl = QtGui.QLabel(i,self)
             self.lvl = QtGui.QLabel(str(j),self)
             self.lbl = self.lbl.move(50, count*12)
             self.lvl = self.lvl.move(250,count*12)
             count = count + 1
-        
-              
+        """Create screen where word frequecies will be displayed"""
         self.setGeometry(450,450,400,300)
         self.setWindowTitle("Word frequencies")
         self.show()
 
 
 def main(argv):
+    """Opening file and count+return the 20 most frequent words"""
     if len(argv) == 2:
         infile = open(argv[1])
         c = Counter()
